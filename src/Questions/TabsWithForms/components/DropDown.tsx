4dcdc5ee65fc965729fcc309ae23bb16d1b9de7e
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro-2.local>
2025-04-05 21:11:14 +0530

    completino of form validation

--- src/Questions/TabsWithForms/components/DropDown.tsx
+++ src/Questions/TabsWithForms/components/DropDown.tsx
@@ -12,16 +12,42 @@ const DropDown = ({
   minLength,
   options,
   handleChange,
+}: {
+  label: string;
+  type: string;
+  name: string;
+  value: string;
+  onChange?: any;
+  error?: any;
+  placeholder?: string;
+  maxLength?: any;
+  minLength?: any;
+  options?: any;
+  handleChange?: any;
 }) => {
   return (
-    <div className="input-field">
+    <div
+      className="input-field "
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        width: "40%",
+        margin: 20,
+      }}
+    >
       {label && <label htmlFor={name}>{label}</label>}
-      <select name={name} value={value} onChange={handleChange}>
+      <select
+        name={name}
+        value={value}
+        onChange={(e) => handleChange?.(e.target.name, e.target.value)}
+        style={{ padding: 10 }}
+      >
         <option value="" disabled>
           Select an option
         </option>
         {options?.map((option) => (
-          <option key={option.id} value={option.value}>
+          <option key={option.id} value={option.value} style={{ padding: 20 }}>
             {option.value}
           </option>
         ))}
